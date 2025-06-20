f61803a5c952f92a1bfcad894a94d1ddbe1c33f5
sSebster <sknet@live.fr>
2025-06-20 14:12:33 +0200

    Soluce T1 prof

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,68 @@
+#include "glm/ext/scalar_constants.hpp"
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
+
+float easeInOut(float x, float power)
+{
+    if (x < 0.5)
+    {
+        return 0.5 * pow(2 * x, power);
+    }
+    else
+    {
+        return 1 - 0.5 * pow(2 * (1 - x), power);
+    }
+}
+
+struct Particle {
+    glm::vec2 position{
+        utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()),
+        utils::rand(-1.f, +1.f),
+    };
+
+    glm::vec2 velocity;
+
+    float mass{utils::rand(1.f, 2.f)};
+
+    float age{0.f};
+    float lifespan{utils::rand(3.f, 5.f)};
+
+    glm::vec3 start_color{
+        utils::rand(0.f, 1.f),
+        utils::rand(0.f, 1.f),
+        utils::rand(0.f, 1.f),
+    };
+    glm::vec3 end_color{
+        utils::rand(0.f, 1.f),
+        utils::rand(0.f, 1.f),
+        utils::rand(0.f, 1.f),
+    };
+
+    glm::vec3 color() const
+    {
+        return glm::mix(start_color, end_color, easeInOut(relative_age(), 4.f));
+    }
+
+    float radius() const
+    {
+        return std::min(lifespan - age, 2.f) / 2.f * 0.03f;
+    }
+
+    float relative_age() const
+    {
+        return age / lifespan;
+    }
+
+    Particle()
+    {
+        float const initial_angle = utils::rand(0.f, 2.f * glm::pi<float>());
+
+        velocity = {
+            0.2f * std::cos(initial_angle),
+            0.2f * std::sin(initial_angle),
+        };
+    }
+};
 
 int main()
 {
@@ -8,56 +71,35 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-
-    // TODO: create an array of particles
-    // Définition de la structure Particle.
-    struct Particle {
-        glm::vec2 position; // Position 2D de la particule (x, y)
-        glm::vec2 velocity; // Vitesse 2D (vx, vy)
-        float life;         // Durée de vie de la particule
-        glm::vec4 color;    // Couleur (R, G, B, Alpha)
-    };
-
-    // Nombre maximal de particules
-    const int MAX_PARTICLES = 1000;
-
-    // Tableau de particules
-    std::vector<Particle> particles(MAX_PARTICLES);
-
-    // Initialisation des particules
-    for (auto& particle : particles) {
-        particle.position = glm::vec2(    // Position aléatoire dans l'écran
-            static_cast<float>(rand()) / RAND_MAX * 2.0f - 1.0f,   // X entre -1 et 1
-            static_cast<float>(rand()) / RAND_MAX * 2.0f - 1.0f    // Y entre -1 et 1
-        );
-        particle.velocity = glm::vec2(   // Vitesse aléatoire
-            static_cast<float>(rand()) / RAND_MAX * 0.2f - 0.1f,   // Vx entre -0.1 et 0.1
-            static_cast<float>(rand()) / RAND_MAX * 0.2f - 0.1f    // Vy entre -0.1 et 0.1
-        );
-        particle.life = static_cast<float>(rand()) / RAND_MAX * 5.0f;   // Vie entre 0 et 5 secondes
-        particle.color = glm::vec4(      // Couleur aléatoire translucide
-            static_cast<float>(rand()) / RAND_MAX, // Rouge
-            static_cast<float>(rand()) / RAND_MAX, // Vert
-            static_cast<float>(rand()) / RAND_MAX, // Bleu
-            1.0f                              // Alpha initial à 1.0 (complètement opaque)
-        );
-    }
-
-    //-----------------------
+    std::vector<Particle> particles(100);
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // TODO update particles
-        // TODO render particles
+        for (auto& particle : particles)
+        {
+            particle.age += gl::delta_time_in_seconds();
 
-        // Afficher toutes les particules
-        for (const auto& particle : particles) {
-            // Valeur hardcodée pour le rayon
-            const float radius = 0.01f;
-            utils::draw_disk(particle.position, radius, particle.color);
+            auto forces = glm::vec2{0.f};
+
+            // Gravity
+            // forces += glm::vec2{0.f, -1.f} * particle.mass;
+
+            // Air friction
+            forces += -particle.velocity * 1.f;
+
+            // Follow mouse
+            // forces += (gl::mouse_position() - particle.position);
+
+            particle.velocity += forces / particle.mass * gl::delta_time_in_seconds();
+            particle.position += particle.velocity * gl::delta_time_in_seconds();
         }
+
+        std::erase_if(particles, [&](Particle const& particle) { return particle.age > particle.lifespan; });
+
+        for (auto const& particle : particles)
+            utils::draw_disk(particle.position, particle.radius(), glm::vec4{particle.color(), 1.f});
     }
 }
