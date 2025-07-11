7cee3209342f7f1d07e70828c3031b2f166daa37
sSebster <sknet@live.fr>
2025-07-11 14:42:14 +0200

    CARRé

--- src/main.cpp
+++ src/main.cpp
@@ -73,12 +73,12 @@ struct Particle {
 
     glm::vec3 color() const
     {
-        return glm::mix(start_color, end_color, easeInOut(relative_age(), 4.f));
+        return start_color;
     }
 
     float radius() const
     {
-        return std::min(lifespan - age, 2.f) / 2.f * 0.03f;
+        return 0.015f;
     }
 
     float relative_age() const
@@ -117,9 +117,11 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    std::vector<Particle> particles(100);
+    std::vector<Particle> particles(3000);
     std::vector<Segment> segments;
-    for (int i = 0; i < 5; ++i)
+
+
+    /*for (int i = 0; i < 5; ++i)
     {
         glm::vec2 A{
             utils::rand(-gl::window_aspect_ratio(), +gl::window_aspect_ratio()),
@@ -130,6 +132,13 @@ int main()
             utils::rand(-1.f, +1.f),
         };
         segments.push_back({A, B});
+    }*/
+
+    for (auto& particle : particles)
+    {
+        glm::vec2 xV(-0.5f,0.5f);
+        glm::vec2 yV(-0.5f,0.5f);
+        particle.position = glm::vec2(utils::rand(0,1)*xV.x,utils::rand(0,1)*yV.y);
     }
 
     while (gl::window_is_open())
@@ -137,24 +146,25 @@ int main()
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        for (auto const& particle : particles) utils::draw_disk(particle.position, particle.radius(), glm::vec4{particle.color(), 1.f});
         for (auto& particle : particles)
         {
-            glm::vec2 previous = particle.position;
+            //glm::vec2 previous = particle.position;
             //particle.age += gl::delta_time_in_seconds();
 
-            auto forces = glm::vec2{0.f};
+            //auto forces = glm::vec2{0.f};
 
             // Gravity
             // forces += glm::vec2{0.f, -1.f} * particle.mass;
 
             // Air friction
-            forces += -particle.velocity * 1.f;
+            //forces += -particle.velocity * 1.f;
 
             // Follow mouse
             // forces += (gl::mouse_position() - particle.position);
 
 
-            glm::vec2 new_pos = particle.position + particle.velocity * gl::delta_time_in_seconds();
+            /*glm::vec2 new_pos = particle.position + particle.velocity * gl::delta_time_in_seconds();
             for (auto const &seg : segments)
             {
                 // Utilisation correcte de intersect_segments pour détecter une collision avec un segment
@@ -172,27 +182,25 @@ int main()
                 particle.position = new_pos;
                 particle.velocity += forces / particle.mass * gl::delta_time_in_seconds();
                 particle.position += particle.velocity * gl::delta_time_in_seconds();
-            }
+            }*/
 
-            std::erase_if(particles, [&](Particle const& particle) { return particle.age > particle.lifespan; });
+            //std::erase_if(particles, [&](Particle const& particle) { return particle.age > particle.lifespan; });
 
-            for (auto const& particle : particles)
-                utils::draw_disk(particle.position, particle.radius(), glm::vec4{particle.color(), 1.f});
 
-            auto A = glm::vec2(-1, 0);
+            /*auto A = glm::vec2(-1, 0);
             auto B = glm::vec2(1, 0);
             auto C = glm::vec2(0, -0.75f);
-            auto D = gl::mouse_position();
+            auto D = gl::mouse_position();*/
 
-            if (auto res = intersect_segments(A, B, C, D)) {
+            //if (auto res = intersect_segments(A, B, C, D)) {
                 // draw_point(glm::vec2 pos, float size, glm::vec4 const& color)
-                utils::draw_disk(res->point, 0.02f, glm::vec4{0.f, 1.f, 0.f, 1.f});
-            }
+                //utils::draw_disk(res->point, 0.02f, glm::vec4{0.f, 1.f, 0.f, 1.f});
+            //}
         }
 
         //Line
         //draw_line(glm::vec2 start, glm::vec2 end, float thickness, glm::vec4 const& color)
-        utils::draw_line(glm::vec2(-1,0), glm::vec2(1,0), 0.01, glm::vec4{1.f, 0.f, 0.f, 1.f});
-        utils::draw_line(glm::vec2(0,-0.75), gl::mouse_position(), 0.01, glm::vec4{1.f, 1.f, 1.f, 1.f});
+        //utils::draw_line(glm::vec2(-1,0), glm::vec2(1,0), 0.01, glm::vec4{1.f, 0.f, 0.f, 1.f});
+        //utils::draw_line(glm::vec2(0,-0.75), gl::mouse_position(), 0.01, glm::vec4{1.f, 1.f, 1.f, 1.f});
     }
 }
